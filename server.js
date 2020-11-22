a238c2195c61c06b60ea41b546b1074bb46a3c0e
singleshade <54930671+singleshade@users.noreply.github.com>
2020-11-22 16:36:19 +0530

    routes & database connection

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
-//this will look for an environment variable called port when its deployed and on local it will run on port no. 5000.
-const PORT = process.env.PORT || 5000; 
+
+const connectDB = require('./config/database');
+connectDB();
+
 
 app.get('/', (req, res) => res.send('API Running'));
+
+// //Define routes
+app.use('/api/users', require('./routes/api/users') );
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
+const PORT = process.env.PORT || 5000;//this will look for an environment variable called port when its deployed and on local it will run on port no. 5000.
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
